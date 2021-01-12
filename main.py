a26800cf1c611b9d2a30077b570ff3e83dfb3a34
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2021-01-11 22:35:08 -0300

    add feature key down and refactor

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from simple_term_menu import TerminalMenu
 import time
-from utils import up_next, verify, clear
-from levels import NAMES, LESSONS
+
+from getkey import getkey, keys
+from simple_term_menu import TerminalMenu
+
+from utils import (
+    up_next,
+    verify,
+    clear,
+    verify_event_key_down,
+    verify_key_down,
+    create_title
+)
+from lessons import NAMES, LESSONS
 from level import level
 
 
-MAIN_MENU_TITLE = "                  VIM TRAINER                 \n\n" + "What's your Vim skill level right now?"
+MAIN_MENU_TITLE = "                  VIM TRAINER                 \n\n" + "What's your Vim skill level right now?\n"
 MAIN_MENU_ITEMS = ["I'm a beginner", "I'm experienced, but I want to learn more", "I'm tired, I'm going out"]
 MAIN_MENU_CURSOR = "> "
 MAIN_MENU_CURSOR_STYLE = ("fg_red", "bold")
@@ -14,35 +24,50 @@ MAIN_MENU_EXIT = False
 
 
 def start_beginner():
-    for i in range(0, 11):
+    for i in range(0, 10):
         level_i = level(NAMES[i], LESSONS[i])
         start_level(level_i)
 
 
 def start_level(level):
     up_next(level)
-    input("-> PRESS ENTER TO START THIS LEVEL")
+    input("-> PRESS ENTER TO START THIS LEVEL <-")
     clear()
 
     for i in range(4):
         lesson = level.get_lesson()
-        create_lesson(lesson)
+        show_lesson(lesson)
 
-    print(f"YOU COMPLETED {level.name}")
+    print(f"\n-> YOU COMPLETED {level.name} <-\n")
 
 
-def create_lesson(lesson):
+def show_lesson(lesson):
     flag = False
+    first_lesson = True
+    lesson_title = f" >>  {lesson[0]}" if (len(lesson[0])%2==0) else f" >> {lesson[0]}"
     while not flag:
         clear()
-        print("    " + "\u001b[46m    " + lesson[0] + "    \u001b[0m")
-        answer = input("\u001b[31m" + "> " + "\u001b[0m")
-        flag = verify(answer, lesson[1])
-        if(flag):
-            print("\u001b[42m" + (20*" ") + "RIGHT" + (20*" "))
+        print(create_title(lesson_title))
+
+        if(not first_lesson):
+            print(create_title("WRONG", "\u001b[41m"))
+            print(create_title("TRY AGAIN", "\u001b[41m"))
         else:
-            print("\u001b[41m" + (20*" ") + "WRONG" + (20*" "))
-        input((11*" ") + "PRESS ENTER TO CONTINUE" + (11*" ") + "\u001b[0m")
+            first_lesson = False
+            print("\n")
+
+        if(verify_event_key_down(lesson[1])):
+            answer = getkey()
+            answer = str(keys.name(answer))
+            flag = verify_key_down(answer, lesson[1])
+        else:
+            answer = input("\u001b[31m" + "> " + "\u001b[0m")
+            flag = verify(answer, lesson[1])
+
+    clear()
+    print(create_title(lesson_title))
+    print(create_title("RIGHT", "\u001b[42m"))
+    input(create_title("PRESS ENTER TO CONTINUE", "\u001b[42m"))
     clear()
 
 
@@ -55,14 +80,13 @@ if(__name__ == '__main__'):
                          cycle_cursor=True,
                          clear_screen=True)
 
-
     while not MAIN_MENU_EXIT:
         menu_var = main_menu.show()
         if menu_var == 0:
             start_beginner()
         elif menu_var == 1:
-            print("b")
-            time.sleep(5)
+            print("This feature has not yet been implemented :(")
+            input("PRESS ENTER TO EXIT")
         else:
             MAIN_MENU_EXIT = True
 
