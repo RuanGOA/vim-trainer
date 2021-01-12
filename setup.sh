dddd844e7b58c63276cfe82ad06e58017616d052
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2021-01-11 23:05:58 -0300

    add dependency check

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
+if ! $(which python3) -eq "python3 not found" ;
+then 
+  sudo install apt install python3.8
+fi
+
+if ! $(which pip3) -eq "pip3 not found" ;
+then 
+  sudo apt install python3-pip
+fi
+
 pip3 install -r requirements.txt
 
