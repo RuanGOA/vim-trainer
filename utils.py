4109aa14ecd3d66acba0ff93bd3d5da73f269186
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2021-01-14 19:43:21 -0300

    utils refactor

--- utils.py
+++ utils.py
@@ -1,15 +1,14 @@
 from os import system as sys, name as nm
 
 
-
 def up_next(level):
     print(level.name)
     for text, lesson_list in level.lessons.items():
         print("    " + text, end=": ")
-        for i in range(len(lesson_list)):
-            if(i > 0):
+        for i, answer in enumerate(lesson_list):
+            if i > 0:
                 print(" or ", end="")
-            print("\u001b[46m " + lesson_to_string(lesson_list[i]) + " \u001b[0m", end="")
+            print("\u001b[46m " + lesson_to_string(answer) + " \u001b[0m", end="")
         print()
     print()
 
@@ -17,43 +16,18 @@ def up_next(level):
 def lesson_to_string(lesson):
     result = ""
     for item in lesson:
-        if ("CTRL_" in item):
+        if "CTRL_" in item:
             result += "ctrl+"
             result += item[-1].lower()
         else:
             result += item
-        """
-        if (item == "COLON"):
-            result += ":"
-        elif (item == "ESC"):
-            result += "escape"
-        elif (item == "EXCLAMATION"):
-            result += "!"
-        elif ("SHIFT" in item):
-            result += item[-1].upper()
-        elif ("CTRL" in item):
-            cache = item[5:]
-            result += "ctrl+"
-            if(cache == "LEFT BRACKET"):
-                result += "["
-            else:
-                result += item[-1].lower()
-        elif (item == "DOLLAR"):
-            result += "$"
-        elif (item == "CARET"):
-            result += "^"
-        elif (item[0] == "N" and item[1].isnumeric()):
-            result += item[1]
-        else:
-            result += item.lower()
-        """
     return result
 
 
-def verify(answer, right_answers):
-    for el in right_answers:
-        stri = lesson_to_string(el)
-        if answer == stri:
+def verify(user_answer, right_answers):
+    for answer in right_answers:
+        stri = lesson_to_string(answer)
+        if user_answer == stri:
             return True
     return False
 
